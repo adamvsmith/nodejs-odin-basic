3e7940b02f25509d75763c0ef9f172e215a7981e
adamvsmith <63735232+adamvsmith@users.noreply.github.com>
2020-05-21 21:30:29 +0200

    replaced PAGES with req.url.slice

--- index.js
+++ index.js
@@ -2,22 +2,18 @@
 const http = require('http');
 const fs = require('fs');
 
-const PAGES = {
-    '/about':       'about.html',
-    '/contact-me':  'contact-me.html',
-    '/':            'index.html'
-};
-
 http.createServer((req, res) => {
-  let page = PAGES[req.url];
-  if (page === undefined) {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("404 Not Found");
-  }
-  else {
-    fs.readFile(page, function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  }
+  const file = (req.url === '/') ? 'index.html' : `${req.url.slice(1)}.html`
+  fs.readFile(file, (err, data) => {
+    if(err){
+      return fs.readFile('404.html', (err, data) => {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.end(data)
+      })
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data)
+    }
+  })
 }).listen(8080);
